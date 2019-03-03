524375becc1e865d079fc29d17df488e2be18eec
ximing <807754634@qq.com>
2019-03-03 22:03:20 +0800

    fix: fix branch

--- build/deploy.sh
+++ build/deploy.sh
@@ -6,6 +6,7 @@ git add .
 dateAndMonth=`date "+%b %Y"`
 git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 git remote add origin https://ximing:${GH_TOKEN}@github.com/ximing/playground.git > /dev/null 2>&1
+git checkout -b gh-pages
 git push origin gh-pages --force
 curl
 curl -H "Content-Type:application/json" -X POST -d '{"token":"'"$DX_TOKEN"'", "text":"[playground] travis build success","id":"64011782718"}' https://mall.meituan.com/wx/service/dx/sendToGroup
